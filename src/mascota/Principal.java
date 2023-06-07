7f02a47c9a0c091d8558aee9b22a52c12b2fd3b6
Sergio Diaz <sydiazcastro@gmail.com>
2023-06-06 21:58:30 -0500

    añadir

---
+++ src/mascota/Principal.java
@@ -0,0 +1,13 @@
+package mascota;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        Proceso MisProcesos = new Proceso();
+        MisProcesos.iniciar();
+
+    }
+
+
+}
